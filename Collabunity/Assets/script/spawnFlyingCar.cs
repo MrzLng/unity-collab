ecb6fc77e0f8ecde0023c1eae3e0b16acfe96dc5
MrzLng <99460974+MrzLng@users.noreply.github.com>
2023-06-22 22:07:08 +0800

    hope no corrupt

---
+++ Collabunity/Assets/script/spawnFlyingCar.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnFlyingCar : MonoBehaviour
+{/*
+    public Transform a;
+    public Transform b;
+    public Transform c;
+    public Transform d;
+    public Transform e;
+    public Transform f;
+    public Transform g;
+    public Transform h;
+    public GameObject car;
+    private double t;
+
+    Transform SpawnPoints = new List<Transform>();
+        SpawnPoints.Add(a);
+        SpawnPoints.Add(b);
+        SpawnPoints.Add(c);
+        SpawnPoints.Add(d);
+        SpawnPoints.Add(e);
+        SpawnPoints.Add(f);
+        SpawnPoints.Add(g);
+        SpawnPoints.Add(h);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        if (Time.time - 5 > t)
+        {
+            transform.Position = spawns[Random.Range(0, 9)].Position;
+            transform.Orientation = spawns[Random.Range(0, 9)].Orientation;
+
+        }
+    }*/
+}
