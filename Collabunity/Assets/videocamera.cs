51125330123bf5f8765bb8f5e114593e6bc76da0
Jeff <realjefff2000@gmail.com>
2023-06-29 21:03:26 +0800

    removed unnecessary things

--- Collabunity/Assets/videocamera.cs
+++ Collabunity/Assets/videocamera.cs
@@ -7,7 +7,7 @@ public class videocamera : MonoBehaviour
     GameObject npc;
     private bool zooming = false;
     private float percentage = 0f;
-    private Vector3 currentheight = new Vector3(0f, 60f, 0f);
+    private Vector3 currentheight = new Vector3(0f, 30f, 0f);
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +21,7 @@ public class videocamera : MonoBehaviour
         {
             zooming = true;
         }
-        if (zooming) { currentheight = Vector3.Lerp(currentheight, new Vector3(0f, 30f, 0f), percentage); percentage += 0.2f * Time.deltaTime; }
+        if (zooming) { currentheight = Vector3.Lerp(currentheight, new Vector3(0f, 15f, 0f), percentage); percentage += 0.2f * Time.deltaTime; }
         transform.position = npc.transform.position + currentheight;
         Debug.Log("AAAAAAAAAAAAAAA "+transform.position);
         transform.rotation = Quaternion.LookRotation(npc.transform.position - transform.position);
