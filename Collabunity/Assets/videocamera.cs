227dce16e72a4d30113d3d773d3cf3b95b409508
Jeff <realjefff2000@gmail.com>
2023-06-29 23:39:06 +0800

    update 69

--- Collabunity/Assets/videocamera.cs
+++ Collabunity/Assets/videocamera.cs
@@ -7,23 +7,17 @@ public class videocamera : MonoBehaviour
     GameObject npc;
     private bool zooming = false;
     private float percentage = 0f;
-    private Vector3 currentheight = new Vector3(-5f, 1f, -0.5f);
+    private Vector3 currentheight = new Vector3(-79f, 15.4f, -85f);
     // Start is called before the first frame update
     void Start()
     {
-        npc = GameObject.Find("/SPECIALNPCPOD/NPC");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (npc.transform.parent == null)
-        {
-            zooming = true;
-        }
-        if (zooming) { currentheight = Vector3.Lerp(currentheight, new Vector3(0f, 3f, -5f), percentage); percentage += 0.02f * Time.deltaTime; }
-        transform.position = npc.transform.position + currentheight;
-        Debug.Log("AAAAAAAAAAAAAAA "+transform.position);
-        transform.rotation = Quaternion.LookRotation(npc.transform.position - transform.position);
+        if (Input.GetKey("q"))zooming = true;
+        if (zooming) { transform.position = Vector3.Lerp(currentheight, new Vector3(-28f, 2f, -85f), percentage); percentage += 0.2f * Time.deltaTime; }
+        transform.rotation = Quaternion.LookRotation(new Vector3(-54.31f, 8.15f, -101.63f) - transform.position);
     }
 }
